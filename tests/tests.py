9608dd5aeddda91b38e4bd61940bd5cb82c1c0f7
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-03-07 13:01:16 -0800

    Create proper tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,21 +1,49 @@
+import asyncio
+import unittest
+import docker
+import time
+
 import aerospike
 from aerospike_async import AsyncClient, Key
-import asyncio
 
-config = {
-    "hosts": [("127.0.0.1", 3000)]
-}
-client = aerospike.client(config)
+class TestAsyncClient(unittest.TestCase):
+    def setUp(self):
+        self.docker_client = docker.from_env()
+        self.server_container = self.docker_client.containers.run(
+            image="aerospike/aerospike-server",
+            ports={
+                3000: 3000,
+                3001: 3001,
+                3002: 3002
+            },
+            detach=True
+        )
 
-key_tuple = ("test", "demo", "1")
-client.put(key_tuple, {"bin": "value"})
+        print("Waiting for server to start...")
+        time.sleep(3)
+        print("Running tests...")
 
-key = Key("test", "demo", "1")
-response = asyncio.run(AsyncClient.get(key))
-print(response)
+        config = {
+            "hosts": [("127.0.0.1", 3000)]
+        }
+        self.client = aerospike.client(config)
+        self.key_tuple = ("test", "demo", "1")
+        self.client.put(self.key_tuple, {"bin": "value"})
 
-record = client.get(key_tuple)
-print(record)
+    def tearDown(self):
+        self.client.remove(self.key_tuple)
+        self.client.close()
 
-client.remove(key_tuple)
-client.close()
+        self.server_container.stop()
+        self.server_container.remove()
+
+        self.docker_client.close()
+
+    def test_get_str(self):
+        key = Key("test", "demo", "1")
+        response = asyncio.run(AsyncClient.get(key))
+        _, _, bins = self.client.get(self.key_tuple)
+        self.assertEqual(response, bins)
+
+if __name__ == '__main__':
+    unittest.main()
