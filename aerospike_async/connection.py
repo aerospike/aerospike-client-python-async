cf2afce27f3f15b835fd67be58902adcc46616e4
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-05-11 11:40:44 -0700

    Working async info

--- aerospike_async/connection.py
+++ aerospike_async/connection.py
@@ -1,35 +1,39 @@
 import time
 import socket
-from tornado.iostream import IOStream
 import asyncio
 
 class Connection:
     last_time_used: int # TODO
     timeout: float
-    stream: IOStream
+    s_reader: asyncio.StreamReader
+    s_writer: asyncio.StreamWriter
 
-    def __init__(self, stream: IOStream):
-        self.stream = stream
+    def __init__(self, s_reader, s_writer):
+        self.s_reader = s_reader
+        self.s_writer = s_writer
 
     @staticmethod
     async def new(policy, address: str, port: int):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        conn = Connection(IOStream(sock))
+        s_reader, s_writer = await asyncio.open_connection(address, port)
+        conn = Connection(s_reader, s_writer)
         socket_address = (address, port)
         # TODO: set timeout for connect
-        await conn.stream.connect(socket_address)
         return conn
 
     def set_timeout(self, timeout: float):
         self.timeout = timeout
 
-    async def read(self, buf: bytearray):
+    async def read(self, num_bytes: int) -> bytes:
         # TODO: handle exceptions here
-        self.stream.socket.settimeout(self.timeout)
-        await self.stream.read_into(buf)
+        # TODO: take care of setting timeout
+        # self.stream.socket.settimeout(self.timeout)
+        res = await self.s_reader.readexactly(num_bytes)
         self.last_time_used = time.time_ns()
+        return res
 
     async def write(self, buf: bytes):
-        self.stream.socket.settimeout(self.timeout)
-        await self.stream.write(buf)
+        # TODO: take care of setting timeout
+        # self.stream.socket.settimeout(self.timeout)
+        self.s_writer.write(buf)
+        await self.s_writer.drain()
         self.last_time_used = time.time_ns()
--- aerospike_async/info.py
+++ aerospike_async/info.py
@@ -7,7 +7,7 @@ PROTOCOL_VERSION = 2
 AS_INFO = 1
 
 class Info:
-    buffer: memoryview
+    buffer: bytearray
 
     def __init__(self, commands: list[str]):
         request = ""
@@ -16,37 +16,37 @@ class Info:
             request += "\n"
 
         # TODO: use buffer pool (preferably without a library)
-        print(len(request.encode("utf-8")))
-        print(request)
+        # print(len(request.encode("utf-8")))
+        # print(request)
         size = len(request.encode("utf-8"))
-        self.buffer = memoryview(bytearray(8 + size))
+        self.buffer = bytearray(8 + size)
         # TODO: 
         self.buffer[:2] = bytes([PROTOCOL_VERSION, AS_INFO])
         self.buffer[2:8] = size.to_bytes(length=6, byteorder='big')
         self.buffer[8:] = bytearray(request, encoding="utf-8")
-        print(self.buffer)
+        # print(self.buffer)
 
-    async def send_command(self, policy: InfoPolicy, conn: Connection):
+    async def send_command(self, policy: InfoPolicy, conn: Connection) -> bytes:
         conn.set_timeout(policy.timeout)
         await conn.write(self.buffer)
-        self.buffer[:8].tobytes()
-        await conn.read()
-        print(self.buffer[:8])
-        size = int.from_bytes(self.buffer[2:8], byteorder='big')
-        print("Results size:", size)
-        await conn.read(self.buffer[:size])
-        print("Results:", self.buffer[:size])
+        buf = await conn.read(8)
+        # print(buf)
+        size = int.from_bytes(buf[2:8], byteorder='big')
+        # print("Results size:", size)
+        buf = await conn.read(size)
+        # print("Results:", buf)
+        return buf
 
     @staticmethod
     async def request(policy: InfoPolicy, conn: Connection, commands: list[str]):
         info = Info(commands)
-        await info.send_command(policy, conn)
-        return info.parse_multi_response()
+        buf = await info.send_command(policy, conn)
+        return info.parse_multi_response(buf)
 
-    def parse_multi_response(self) -> dict[str, str]:
+    def parse_multi_response(self, buf: bytes) -> dict[str, str]:
         # If command is invalid, the command and value for it will not be returned from the server (in the buffer)
         responses = {}
-        res = str(self.buffer, encoding="utf-8")
+        res = str(buf, encoding="utf-8")
         res = res.split()
         for i in range(0, len(res), 2):
             responses[res[i]] = res[i + 1]
