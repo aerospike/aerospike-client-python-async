ad0cabdf4ca42d66fe86ca0e9b9aa93bf2ee6467
juliannguyen4 <109386615+juliannguyen4@users.noreply.github.com>
2023-05-26 14:15:29 -0700

    Add Khosrow's files

--- aerospike_async/host.py
+++ aerospike_async/host.py
@@ -1,6 +1,33 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
+import ipaddress
+import socket
 
 @dataclass
 class Host:
     name: str
     port: int
+    tls_name: str
+
+    async def resolve(self) -> list[Host]:
+        addrs = socket.getaddrinfo(self.name, self.port, family=socket.AF_INET, proto=socket.IPPROTO_TCP)
+        res: list[Host] = []
+        for addr in addrs:
+            _, _, _, _, (ip, port) = addr
+            res.append(Host(ip, port, self.tls_name))
+
+        return res
+
+    def is_loopback(self) -> bool:
+        try:
+            return ipaddress.ip_address(self.name).is_loopback
+        except:
+            return False
+
+    def is_ip(self) -> bool:
+        try:
+            ipaddress.ip_address(self.name)
+            return True
+        except:
+            return False
