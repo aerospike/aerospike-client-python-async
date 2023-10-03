e025629da7d765a22488f8cf15a40372923f7d3e
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2023-10-03 14:35:56 -0700

    Combine connect() and close() test

--- tests/client_test.py
+++ tests/client_test.py
@@ -1,15 +1,9 @@
-import asyncio
 import unittest
 
-from aerospike_async import *
+from aerospike_async import ClientPolicy, new_client
 
 class TestClient(unittest.IsolatedAsyncioTestCase):
     async def test_connect(self):
-        cp = ClientPolicy()
-        c = await new_client(cp, "localhost:3000")
-        self.assertIsNotNone(c)
-
-    async def test_close(self):
         cp = ClientPolicy()
         c = await new_client(cp, "localhost:3000")
         self.assertIsNotNone(c)
