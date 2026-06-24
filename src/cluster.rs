5fea548555622c1f60bf0975c16bfa4e433acf9b
Gagan Mishra <gmishra@aerospike.com>
2026-06-24 13:24:55 -0700

    disable ael on serverside for now

--- src/cluster.rs
+++ src/cluster.rs
@@ -174,7 +174,9 @@ use crate::policies::AdminPolicy;
 
         /// Returns true if server accepts server-compiled AEL on filter field 43 (>= 8.1.3.0).
         pub fn supports_server_compiled_ael(&self) -> bool {
-            self._as.supports_server_compiled_ael()
+            false
+            // TODO: enable this when server changes are merged
+            // self._as.supports_server_compiled_ael()
         }
 
         pub fn __str__(&self) -> String {
