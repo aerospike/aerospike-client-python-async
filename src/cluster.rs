df8c83fd5eb9bdada0c463351a3e597cf9a78178
Gagan Mishra <gmishra@aerospike.com>
2026-07-01 09:37:28 -0700

    hardcode server ael support to true for test purposes

--- src/cluster.rs
+++ src/cluster.rs
@@ -174,7 +174,7 @@ use crate::policies::AdminPolicy;
 
         /// Returns true if server accepts server-compiled AEL on filter field 43 (>= 8.1.3.0).
         pub fn supports_server_compiled_ael(&self) -> bool {
-            false
+            true
             // TODO: enable this when server changes are merged
             // self._as.supports_server_compiled_ael()
         }
